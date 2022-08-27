7a17e86e5c095b50fba83b97f2deab8e8431905b
klim0031 <56181965+klim0031@users.noreply.github.com>
2022-08-27 11:02:18 +0800

    update

--- layers/box_utils.py
+++ layers/box_utils.py
@@ -128,15 +128,15 @@ def encode_v2(matched, priors, variances):
     g_cxcy = (matched[:, :2] + matched[:, 2:4])/2 - priors[:, :2]
     # encode variance
     g_cxcy /= (variances[0] * priors[:, 2:])
-    
+
     # match wh / prior wh
     g_wh = (matched[:, 2:4] - matched[:, :2]) / priors[:, 2:]
     g_wh = torch.log(g_wh) / variances[1]
-    
+
     #
     g_wxy = torch.log( matched[:, 4:6]/ priors[:, 2:] + 0.1 ) / variances[1]
     g_hxy = torch.log( matched[:, 6:8]/ priors[:, 2:] + 0.1 ) / variances[1]
-    
+
     #
     wrist_vec = (matched[:, 8:10] - priors[:, :2])/ (variances[0] * priors[:, 2:])
 
@@ -212,156 +212,153 @@ def log_sum_exp(x):
     return torch.log(torch.sum(torch.exp(x-x_max), 1, keepdim=True)) + x_max
 
 
-# Original author: Francisco Massa:
-# https://github.com/fmassa/object-detection.torch
-# Ported to PyTorch by Max deGroot (02/01/2017)
-def nms_old(boxes, scores, overlap=0.5, top_k=200):
-    """Apply non-maximum suppression at test time to avoid detecting too many
-    overlapping bounding boxes for a given object.
-    Args:
-        boxes: (tensor) The location preds for the img, Shape: [num_priors,4].
-        scores: (tensor) The class predscores for the img, Shape:[num_priors].
-        overlap: (float) The overlap thresh for suppressing unnecessary boxes.
-        top_k: (int) The Maximum number of box preds to consider.
-    Return:
-        The indices of the kept boxes with respect to num_priors.
-    """
-
-    keep = scores.new(scores.size(0)).zero_().long()
-    if boxes.numel() == 0:
-        return keep
-    x1 = boxes[:, 0]
-    y1 = boxes[:, 1]
-    x2 = boxes[:, 2]
-    y2 = boxes[:, 3]
-    area = torch.mul(x2 - x1, y2 - y1)
-    v, idx = scores.sort(0)  # sort in ascending order
-    # I = I[v >= 0.01]
-    idx = idx[-top_k:]  # indices of the top-k largest vals
-    xx1 = boxes.new()
-    yy1 = boxes.new()
-    xx2 = boxes.new()
-    yy2 = boxes.new()
-    w = boxes.new()
-    h = boxes.new()
-
-    # keep = torch.Tensor()
-    count = 0
-    while idx.numel() > 0:
-        i = idx[-1]  # index of current largest val
-        # keep.append(i)
-        keep[count] = i
-        count += 1
-        if idx.size(0) == 1:
-            break
-        idx = idx[:-1]  # remove kept element from view
-        # load bboxes of next highest vals
-        torch.index_select(x1, 0, idx, out=xx1)
-        torch.index_select(y1, 0, idx, out=yy1)
-        torch.index_select(x2, 0, idx, out=xx2)
-        torch.index_select(y2, 0, idx, out=yy2)
-        # store element-wise max with next highest score
-        xx1 = torch.clamp(xx1, min=x1[i])
-        yy1 = torch.clamp(yy1, min=y1[i])
-        xx2 = torch.clamp(xx2, max=x2[i])
-        yy2 = torch.clamp(yy2, max=y2[i])
-        w.resize_as_(xx2)
-        h.resize_as_(yy2)
-        w = xx2 - xx1
-        h = yy2 - yy1
-        # check sizes of xx1 and xx2.. after each iteration
-        w = torch.clamp(w, min=0.0)
-        h = torch.clamp(h, min=0.0)
-        inter = w*h
-        # IoU = i / (area(a) + area(b) - i)
-        rem_areas = torch.index_select(area, 0, idx)  # load remaining areas)
-        union = (rem_areas - inter) + area[i]
-        IoU = inter/union  # store result in iou
-        # keep only elements with an IoU <= overlap
-        idx = idx[IoU.le(overlap)]
-    return keep, count
-
-
-from ._ext import nms
-
-def nms_new(boxes, scores, overlap=0.5, top_k=200):
-    """Apply non-maximum suppression at test time to avoid detecting too many
-    overlapping bounding boxes for a given object.
-    Args:
-        boxes: (tensor) The location preds for the img, Shape: [num_priors,4].
-        scores: (tensor) The class predscores for the img, Shape:[num_priors].
-        overlap: (float) The overlap thresh for suppressing unnecessary boxes.
-        top_k: (int) The Maximum number of box preds to consider.
-    Return:
-        The indices of the kept boxes with respect to num_priors.
-    """
-    # print("nms_old:.>>>>>>>>>>>>>>>>>")
-    # nms_old(boxes, scores, overlap, top_k)
-    # print("nms_new:.>>>>>>>>>>>>>>>>>")
-
-    dets = torch.cat((boxes, scores), dim=1)
-    dets = dets.type(torch.FloatTensor)
-    if boxes.numel() == 0:
-        return keep
-
-
-    scores = dets[:,4]
-    idx = scores.sort(0, descending=True)[1]  # sort in ascending order
-    
-    
-    idx = idx[0:top_k]
-    dets = dets[idx].contiguous()
-
-    scores2 = dets[:,4]
-    idx2 = scores2.sort(0, descending=True)[1]  # sort in ascending order
-
-
-    x1 = dets[:, 0]
-    y1 = dets[:, 1]
-    x2 = dets[:, 2]
-    y2 = dets[:, 3]
-    areas = torch.mul(x2 - x1, y2 - y1)
-
-    # I = I[v >= 0.01]
-    #dets = dets[idx].contiguous()
-    keep = torch.LongTensor(dets.size(0))
-    num_out = torch.LongTensor(1)
-    nms.cpu_nms(keep, num_out, dets, idx2, areas, overlap)
-
-    
-    keep = idx[keep[:num_out[0]]].cuda().contiguous()
-
-    return keep,num_out[0]
-
-
-def nms_new_gpu(boxes, scores, overlap=0.5, top_k=200):
-    """Apply non-maximum suppression at test time to avoid detecting too many
-    overlapping bounding boxes for a given object.
-    Args:
-        boxes: (tensor) The location preds for the img, Shape: [num_priors,4].
-        scores: (tensor) The class predscores for the img, Shape:[num_priors].
-        overlap: (float) The overlap thresh for suppressing unnecessary boxes.
-        top_k: (int) The Maximum number of box preds to consider.
-    Return:
-        The indices of the kept boxes with respect to num_priors.
-    """
-    # print("nms_old:.>>>>>>>>>>>>>>>>>")
-    # nms_old(boxes, scores, overlap, top_k)
-    # print("nms_new:.>>>>>>>>>>>>>>>>>")
-
-    dets = torch.cat((boxes, scores), dim=1)
-    if boxes.numel() == 0:
-        return keep
-    
-    idx = scores.sort(0, descending=True)[1]  # sort in ascending order
-    # I = I[v >= 0.01]
-    dets = dets[idx[0:top_k]].contiguous()
-    keep = torch.LongTensor(dets.size(0))
-    num_out = torch.LongTensor(1)
-    nms.gpu_nms(keep, num_out, dets, overlap)
-
-    #print(keep.size())
-    #print(num_out.size())
-    #print(idx[keep[:num_out[0]].cuda()].contiguous().size())
-
-    return idx[keep[:num_out[0]].cuda()].contiguous().view(-1),num_out[0]
+# # Original author: Francisco Massa:
+# # https://github.com/fmassa/object-detection.torch
+# # Ported to PyTorch by Max deGroot (02/01/2017)
+# def nms_old(boxes, scores, overlap=0.5, top_k=200):
+#     """Apply non-maximum suppression at test time to avoid detecting too many
+#     overlapping bounding boxes for a given object.
+#     Args:
+#         boxes: (tensor) The location preds for the img, Shape: [num_priors,4].
+#         scores: (tensor) The class predscores for the img, Shape:[num_priors].
+#         overlap: (float) The overlap thresh for suppressing unnecessary boxes.
+#         top_k: (int) The Maximum number of box preds to consider.
+#     Return:
+#         The indices of the kept boxes with respect to num_priors.
+#     """
+#
+#     keep = scores.new(scores.size(0)).zero_().long()
+#     if boxes.numel() == 0:
+#         return keep
+#     x1 = boxes[:, 0]
+#     y1 = boxes[:, 1]
+#     x2 = boxes[:, 2]
+#     y2 = boxes[:, 3]
+#     area = torch.mul(x2 - x1, y2 - y1)
+#     v, idx = scores.sort(0)  # sort in ascending order
+#     # I = I[v >= 0.01]
+#     idx = idx[-top_k:]  # indices of the top-k largest vals
+#     xx1 = boxes.new()
+#     yy1 = boxes.new()
+#     xx2 = boxes.new()
+#     yy2 = boxes.new()
+#     w = boxes.new()
+#     h = boxes.new()
+#
+#     # keep = torch.Tensor()
+#     count = 0
+#     while idx.numel() > 0:
+#         i = idx[-1]  # index of current largest val
+#         # keep.append(i)
+#         keep[count] = i
+#         count += 1
+#         if idx.size(0) == 1:
+#             break
+#         idx = idx[:-1]  # remove kept element from view
+#         # load bboxes of next highest vals
+#         torch.index_select(x1, 0, idx, out=xx1)
+#         torch.index_select(y1, 0, idx, out=yy1)
+#         torch.index_select(x2, 0, idx, out=xx2)
+#         torch.index_select(y2, 0, idx, out=yy2)
+#         # store element-wise max with next highest score
+#         xx1 = torch.clamp(xx1, min=x1[i])
+#         yy1 = torch.clamp(yy1, min=y1[i])
+#         xx2 = torch.clamp(xx2, max=x2[i])
+#         yy2 = torch.clamp(yy2, max=y2[i])
+#         w.resize_as_(xx2)
+#         h.resize_as_(yy2)
+#         w = xx2 - xx1
+#         h = yy2 - yy1
+#         # check sizes of xx1 and xx2.. after each iteration
+#         w = torch.clamp(w, min=0.0)
+#         h = torch.clamp(h, min=0.0)
+#         inter = w*h
+#         # IoU = i / (area(a) + area(b) - i)
+#         rem_areas = torch.index_select(area, 0, idx)  # load remaining areas)
+#         union = (rem_areas - inter) + area[i]
+#         IoU = inter/union  # store result in iou
+#         # keep only elements with an IoU <= overlap
+#         idx = idx[IoU.le(overlap)]
+#     return keep, count
+#
+# def nms_new(boxes, scores, overlap=0.5, top_k=200):
+#     """Apply non-maximum suppression at test time to avoid detecting too many
+#     overlapping bounding boxes for a given object.
+#     Args:
+#         boxes: (tensor) The location preds for the img, Shape: [num_priors,4].
+#         scores: (tensor) The class predscores for the img, Shape:[num_priors].
+#         overlap: (float) The overlap thresh for suppressing unnecessary boxes.
+#         top_k: (int) The Maximum number of box preds to consider.
+#     Return:
+#         The indices of the kept boxes with respect to num_priors.
+#     """
+#     # print("nms_old:.>>>>>>>>>>>>>>>>>")
+#     # nms_old(boxes, scores, overlap, top_k)
+#     # print("nms_new:.>>>>>>>>>>>>>>>>>")
+#
+#     dets = torch.cat((boxes, scores), dim=1)
+#     dets = dets.type(torch.FloatTensor)
+#     if boxes.numel() == 0:
+#         return keep
+#
+#
+#     scores = dets[:,4]
+#     idx = scores.sort(0, descending=True)[1]  # sort in ascending order
+#
+#
+#     idx = idx[0:top_k]
+#     dets = dets[idx].contiguous()
+#
+#     scores2 = dets[:,4]
+#     idx2 = scores2.sort(0, descending=True)[1]  # sort in ascending order
+#
+#
+#     x1 = dets[:, 0]
+#     y1 = dets[:, 1]
+#     x2 = dets[:, 2]
+#     y2 = dets[:, 3]
+#     areas = torch.mul(x2 - x1, y2 - y1)
+#
+#     # I = I[v >= 0.01]
+#     #dets = dets[idx].contiguous()
+#     keep = torch.LongTensor(dets.size(0))
+#     num_out = torch.LongTensor(1)
+#     nms.cpu_nms(keep, num_out, dets, idx2, areas, overlap)
+#
+#
+#     keep = idx[keep[:num_out[0]]].cuda().contiguous()
+#
+#     return keep,num_out[0]
+#
+#
+# def nms_new_gpu(boxes, scores, overlap=0.5, top_k=200):
+#     """Apply non-maximum suppression at test time to avoid detecting too many
+#     overlapping bounding boxes for a given object.
+#     Args:
+#         boxes: (tensor) The location preds for the img, Shape: [num_priors,4].
+#         scores: (tensor) The class predscores for the img, Shape:[num_priors].
+#         overlap: (float) The overlap thresh for suppressing unnecessary boxes.
+#         top_k: (int) The Maximum number of box preds to consider.
+#     Return:
+#         The indices of the kept boxes with respect to num_priors.
+#     """
+#     # print("nms_old:.>>>>>>>>>>>>>>>>>")
+#     # nms_old(boxes, scores, overlap, top_k)
+#     # print("nms_new:.>>>>>>>>>>>>>>>>>")
+#
+#     dets = torch.cat((boxes, scores), dim=1)
+#     if boxes.numel() == 0:
+#         return keep
+#
+#     idx = scores.sort(0, descending=True)[1]  # sort in ascending order
+#     # I = I[v >= 0.01]
+#     dets = dets[idx[0:top_k]].contiguous()
+#     keep = torch.LongTensor(dets.size(0))
+#     num_out = torch.LongTensor(1)
+#     nms.gpu_nms(keep, num_out, dets, overlap)
+#
+#     #print(keep.size())
+#     #print(num_out.size())
+#     #print(idx[keep[:num_out[0]].cuda()].contiguous().size())
+#
+#     return idx[keep[:num_out[0]].cuda()].contiguous().view(-1),num_out[0]
---
+++ models/ssd_resnet.py
@@ -0,0 +1,395 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from layers import *
+from data import v2
+import os
+
+from torch.hub import load_state_dict_from_url
+model_url = 'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth'
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+    __constants__ = ['downsample']
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+    __constants__ = ['downsample']
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=1, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
+
+
+def _resnet(arch, block, layers, pretrained, progress, **kwargs):
+    model = ResNet(block, layers, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_url,
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+def resnet101(pretrained=False, progress=True, **kwargs):
+    r"""ResNet-101 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet('resnet101', Bottleneck, [3, 4, 23, 3], pretrained, progress,
+                   **kwargs)
+
+class SSD(nn.Module):
+    def __init__(self, phase, size, base, extras, head, num_classes):
+        super(SSD, self).__init__()
+        self.phase = phase
+        self.num_classes = num_classes
+        self.cfg = (voc, custom)[num_classes == 2]
+        self.priorbox = PriorBox(self.cfg)
+        self.priors = Variable(self.priorbox.forward(), volatile=True)
+        self.size = size
+
+        # SSD network
+        self.resnet = nn.ModuleList(base)
+        # Layer learns to scale the l2 normalized features from conv4_3
+        self.L2Norm = L2Norm(512, 20)
+        self.extras = nn.ModuleList(extras)
+
+        self.loc = nn.ModuleList(head[0])
+        self.conf = nn.ModuleList(head[1])
+
+        if phase == 'test':
+            self.softmax = nn.Softmax(dim=-1)
+            self.detect = Detect(num_classes, 0, 200, 0.01, 0.45)
+
+    def forward(self, x):
+        """Applies network layers and ops on input image(s) x.
+        Args:
+            x: input image or batch of images. Shape: [batch,3,300,300].
+        Return:
+            Depending on phase:
+            test:
+                Variable(tensor) of output class label predictions,
+                confidence score, and corresponding location predictions for
+                each object detected. Shape: [batch,topk,7]
+            train:
+                list of concat outputs from:
+                    1: confidence layers, Shape: [batch*num_priors,num_classes]
+                    2: localization layers, Shape: [batch,num_priors*4]
+                    3: priorbox layers, Shape: [2,num_priors*4]
+        """
+        sources = list()
+        loc = list()
+        conf = list()
+
+        # apply resnet up to layer2
+        for k in range(0,7):
+            x = self.resnet[k](x)
+        sources.append(x)
+
+        # apply resnet up to layer4
+        for k in range(7, len(self.resnet)):
+            x = self.resnet[k](x)
+        sources.append(x)
+        # s = self.L2Norm(x)
+        # sources.append(s)
+
+        # apply extra layers and cache source layer outputs
+        for k, v in enumerate(self.extras):
+            x = F.relu(v(x), inplace=True)
+            if k % 2 == 1:
+                sources.append(x)
+        # apply multibox head to source layers
+        for (x, l, c) in zip(sources, self.loc, self.conf):
+            loc.append(l(x).permute(0, 2, 3, 1).contiguous())
+            conf.append(c(x).permute(0, 2, 3, 1).contiguous())
+
+        loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
+        conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
+        if self.phase == "test":
+            output = self.detect(
+                loc.view(loc.size(0), -1, 4),                   # loc preds
+                self.softmax(conf.view(conf.size(0), -1,
+                             self.num_classes)),                # conf preds
+                self.priors.type(type(x.data))                  # default boxes
+            )
+        else:
+            output = (
+                loc.view(loc.size(0), -1, 4),
+                conf.view(conf.size(0), -1, self.num_classes),
+                self.priors
+            )
+        return output
+
+    def load_weights(self, base_file):
+        other, ext = os.path.splitext(base_file)
+        if ext == '.pkl' or '.pth':
+            print('Loading weights into state dict...')
+            self.load_state_dict(torch.load(base_file,
+                                 map_location=lambda storage, loc: storage))
+            print('Finished!')
+        else:
+            print('Sorry only .pth and .pkl files supported.')
+
+def resnet():
+    resnet = resnet101(pretrained=True)
+    layers = [
+        resnet.conv1,
+        resnet.bn1,
+        resnet.relu,
+        resnet.maxpool,
+        resnet.layer1,
+        resnet.layer2,
+        resnet.layer3,
+        resnet.layer4,
+    ]
+    return layers
+
+def add_extras(cfg, i, batch_norm=False):
+    # Extra layers added to VGG for feature scaling
+    layers = []
+    in_channels = i
+    flag = False
+    for k, v in enumerate(cfg):
+        if in_channels != 'S':
+            if v == 'S':
+                layers += [nn.Conv2d(in_channels, cfg[k + 1],
+                           kernel_size=(1, 3)[flag], stride=2, padding=1)]
+            else:
+                layers += [nn.Conv2d(in_channels, v, kernel_size=(1, 3)[flag])]
+            flag = not flag
+        in_channels = v
+    return layers
+
+
+def multibox(resnet, extra_layers, cfg, num_classes):
+    loc_layers = []
+    conf_layers = []
+    resnet_source = [-2, -1]
+    for k, v in enumerate(resnet_source):
+        if resnet[v] == Bottleneck:
+            loc_layers += [nn.Conv2d(resnet[v][-1].conv3.out_channels,
+                                    cfg[k] * 4, kernel_size=3, padding=1)]
+            conf_layers += [nn.Conv2d(resnet[v][-1].conv3.out_channels,
+                            cfg[k] * num_classes, kernel_size=3, padding=1)]
+        else:
+            loc_layers += [nn.Conv2d(resnet[v][-1].conv2.out_channels,
+                                    cfg[k] * 4, kernel_size=3, padding=1)]
+            conf_layers += [nn.Conv2d(resnet[v][-1].conv2.out_channels,
+                            cfg[k] * num_classes, kernel_size=3, padding=1)]
+    for k, v in enumerate(extra_layers[1::2], 2):
+        loc_layers += [nn.Conv2d(v.out_channels, cfg[k]
+                                 * 4, kernel_size=3, padding=1)]
+        conf_layers += [nn.Conv2d(v.out_channels, cfg[k]
+                                  * num_classes, kernel_size=3, padding=1)]
+    return resnet, extra_layers, (loc_layers, conf_layers)
+
+
+base = {
+    '300': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
+            512, 512, 512],
+    '512': [],
+}
+extras = {
+    '300': [256, 'S', 512, 128, 'S', 256, 128, 256, 128, 256],
+    '512': [],
+}
+mbox = {
+    '300': [4, 6, 6, 6, 4, 4],  # number of boxes per feature map location
+    '512': [],
+}
+
+
+def build_ssd(phase, size=300, num_classes=21):
+    if phase != "test" and phase != "train":
+        print("ERROR: Phase: " + phase + " not recognized")
+        return
+    if size != 300:
+        print("ERROR: You specified size " + repr(size) + ". However, " +
+              "currently only SSD300 (size=300) is supported!")
+        return
+    base_, extras_, head_ = multibox(resnet(),
+                                     add_extras(extras[str(size)], 512),
+                                     mbox[str(size)], num_classes)
+    return SSD(phase, size_, base_, extras_, head_, num_classes)
